ea8f0e1eac28495e10ce7234eebcf0d8555891d2
LovelyHaRa <codetender94@gmail.com>
2020-05-04 18:19:51 +0900

    200504

---
+++ BAEKJOON/BAEKJOON/12015.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+int main(void)
+{
+	int n;
+	cin >> n;
+	vector<int> A;
+	A.push_back(-1);
+	int cnt = 0;
+	while (n--)
+	{
+		int k;
+		cin >> k;
+		// 순증가 한다면 뒤에 삽입
+		if (k > A.back())
+		{
+			A.push_back(k); 
+			cnt++;
+		}
+		else
+		{
+			vector<int>::iterator find = lower_bound(A.begin(), A.end(), k); // 이분 탐색으로 k보다 큰 정수의 위치 반환
+			*find = k; // 바꿔치기
+		}
+	}
+	cout << cnt;
+}
