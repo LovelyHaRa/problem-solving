f851094f8613756c9fc70ea8158bafb7af2bb7cf
LovelyHaRa <codetender94@gmail.com>
2020-10-19 22:03:46 +0900

    201019

--- BAEKJOON/BAEKJOON/16964.cpp
+++ BAEKJOON/BAEKJOON/16964.cpp
@@ -1,40 +1,33 @@
 #include<iostream>
 #include<vector>
+#include<algorithm>
 
 using namespace std;
 
 int n;
 vector<vector<int>> adj; // 인접 리스트 구현
-vector<int> check; // 방문 여부
+vector<bool> check; // 방문 여부
 vector<int> ans; // 정답 인풋
-int idx = 1; // 정답 배열 현재 참조 위치 (1부터 시작)
+vector<int> order; // 방문 순서 인덱스
+vector<int> res; // dfs 수행 결과
 
 // dfs 탐색
 void dfs(int x)
 {
-	// 이미 탐색되었거나 정답 배열이 모두 참조 되었을 경우
-	if (check[x] || idx==n)
-		return;
 	// 탐색 체크
 	check[x] = 1;
-	// 현재 정점으로부터 연결된 정점들을 모두 방문
-	while (idx < n)
+	res.push_back(x); // 탐색 결과 배열에 넣기
+	// 다음 노드 탐색
+	for (int y = 0; y < adj[x].size(); y++)
 	{
-		int y;
-		// 연결된 정점 모두 탐색
-		for (y = 0; y < adj[x].size(); y++)
-		{
-			// 정답 배열의 현재 위치의 값과 정점이 같으면
-			if (ans[idx] == adj[x][y])
-			{
-				idx++; // 정답 배열 참조위치 증가
-				dfs(adj[x][y]);  // dfs 재귀 탐색
-				break; // 정점을 찾았으므로 멈춤
-			}
-		}
-		// 정점을 못찾은 경우 멈춤
-		if (y == adj[x].size()) break;
+		int next = adj[x][y];
+		if (!check[next]) dfs(next);
 	}
+}
+
+bool compare(int a, int b)
+{
+	return order[a] < order[b];
 }
 
 int main(void)
@@ -47,14 +40,23 @@ int main(void)
 	{
 		int a, b;
 		cin >> a >> b;
+		a--; b--; // 정점을 0부터 시작
 		adj[a].push_back(b);
 		adj[b].push_back(a);
 	}
 	ans.resize(n);
+	order.resize(n);
 	for (int i = 0; i < n; i++)
+	{
 		cin >> ans[i];
+		ans[i]--; // 정점을 0부터 시작
+		order[ans[i]] = i; // 기록된 정점의 순서를 저장
+	}
+	// 인접 리스트의 정점을 기록된 정점의 순서로 정렬
+	for (int i = 0; i < n; i++)
+		sort(adj[i].begin(), adj[i].end(), compare);
 	// 1부터 dfs 탐색
-	dfs(1);
+	dfs(0);
 	// 정답배열이 모두 탐색되었으면 1 아니면 0
-	cout << (idx == n) << '\n';
+	cout << (res == ans) << '\n';
 }
